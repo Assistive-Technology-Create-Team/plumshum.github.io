6f6de4139f998ab6bc64ed79d2ed27151eda4d2a
Hannah Zhou <hannahyunzizhou@gmail.com>
2023-01-16 09:58:48 -0500

    FIles added
    Instructions - to set up hardware and software
    Sensordata.py - to collect raspberry pi data with up and down signals
    sensordataML.ipynb - jupyter notebook to ML on collected data

---
+++ sensordata.py
@@ -0,0 +1,39 @@
+from sense_hat import SenseHat
+filename = input ("Enter a filename: ")
+
+sense = SenseHat()
+import time
+collecting = False
+pressed = False
+while True:
+    acceleration = sense.get_accelerometer_raw()
+    pressed = False
+    for event in sense.stick.get_events():
+        if event.action == "pressed":
+            pressed = True
+            print("The joystick was pressed")
+    
+    if pressed == True:
+        if collecting == False:
+            collecting = True
+        else:
+            collecting = False
+            print("Not collecting data")
+    pressed = False
+    if collecting == True:
+        print("Collecting data")
+        x = acceleration['x']
+        y = acceleration['y']
+        z = acceleration['z']
+
+        x=round(x*1000, 0)
+        y=round(y*1000, 0)
+        z=round(z*1000, 0)
+
+        print("x={0}, y={1}, z={2}".format(x, y, z))
+        # write x, y, z to a file
+        with open(filename + ".txt", "a") as f:
+            f.write("{0},{1},{2}".format(x, y, z))
+            # write a new line
+            f.write("\n")
+        time.sleep(1)
