6d1329d9062b2fe117b51efd996f735366dff98f
Hannah Zhou <hannahyunzizhou@gmail.com>
2023-02-12 11:21:55 -0500

    script to connect ML and Raspberry PI

---
+++ script.py
@@ -0,0 +1,25 @@
+from sense_hat import SenseHat
+from sklearn.metrics import accuracy_score
+import tensorflow as tf
+sense = SenseHat()
+# open csv files and read the data
+
+
+# load the model from saved_model
+model = tf.keras.models.load_model('saved_model/my_model')
+# predict the outputs using the model
+predictions = model.predict(x_test)
+  # x_test : the data collected  from the accelerometer
+  # predicted : the labels and the confidence level of the prediction
+# print the accuracy
+print("Accuracy: ", accuracy_score(y_test, predictions))
+# print the confidence level and label of each prediction
+
+correct = 0
+for i in range(len(predictions)):
+    print("Confidence level: ", predictions[i], "Label: ", y_test[i])
+    if predictions[i] > .75:
+        correct+=1
+print("Confidence Level Accurate Percentage:", correct/len(predictions) * 100)
+# filter it based on confidence 
+
