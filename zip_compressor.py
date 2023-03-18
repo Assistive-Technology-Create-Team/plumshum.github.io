3514fca16e8fe35d7c289c8a9f93b2520344579b
Hannah Zhou <hannahyunzizhou@gmail.com>
2023-03-18 11:01:46 -0400

    runs everything in order
    almost

--- zip_compressor.py
+++ zip_compressor.py
@@ -3,6 +3,8 @@ import zipfile
 
 # Turn this directory into a zip file. It will be saved in the same directory as the script. 
 # It is named raspberry_pi_data.zip
+# Manually Upload to Google Drive and Open on Google Colab. 
+# There, run the machine learning script.
 zipf = zipfile.ZipFile('raspberry_pi_data.zip', 'w', zipfile.ZIP_DEFLATED)
 for root, dirs, files in os.walk('raspberry_pi_data'):
     for file in files:
