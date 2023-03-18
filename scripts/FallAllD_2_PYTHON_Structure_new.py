2c76f8bd5be4572e562dfcbbd5a592857cb8024a
Hannah Zhou <hannahyunzizhou@gmail.com>
2023-03-18 12:43:06 -0400

    lol

--- scripts/FallAllD_2_PYTHON_Structure_new.py
+++ scripts/FallAllD_2_PYTHON_Structure_new.py
@@ -1,7 +1,9 @@
-# NEW VERSION: FallAllD files to Python struct
+# FallAllD files to Python struct
 
 # Hannah's Comment: takes data and turns in to panda dataframe, and it's saved in a pickle file.
 #The pickle file is called FallAllD.pkl
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 import os
 from numpy import genfromtxt
 import numpy as np
@@ -28,7 +30,7 @@ l_Acc=[]
 l_Gyr=[]
 #l_Mag=[] not used
 #l_Bar=[] not used
-
+res = pd.DataFrame(columns=['SubjectID', 'Device','ActivityID','TrialNo','Acc_x','Acc_y', 'Acc_z','Gyr_x','Gyr_y', 'Gyr_z'])
 for i in range(LL):
     f_name=FileNames[i]
     SubjectID=int(f_name[1:3])    
@@ -45,24 +47,23 @@ for i in range(LL):
     else: 
         continue
         if (int(f_name[5])==1):
-            Device='Wrist' 
+            Device='Wrist' #Added Code
         else:
             Device='Waist'  
     
-    #Biggest change: Instead of storing accelerometer data as one unit, we split and store it as three separate columns
     df_acc = pd.read_csv(f_name, header=None, sep=',', names=['Acc_x','Acc_y', 'Acc_z'])
     chArr=list(f_name)
     chArr[16]='G'
     f_name="".join(chArr)    
     df_gyr = pd.read_csv(f_name, header=None, sep=',', names=['Gyr_x','Gyr_y', 'Gyr_z'])
-    l_Gyr.append(np.int16(genfromtxt(f_name, delimiter=',')))
-
+    # create a new dataframe called df, df's columns are from the two dataframes df_acc and df_gyr
     df = pd.concat([df_acc, df_gyr], axis=1)
     df['SubjectID'] = SubjectID
     df['Device'] = Device
     df['ActivityID'] = ActivityID
     df['TrialNo'] = TrialNo
-
+    #print(df.head(1))
+    
     #Not used
     #chArr[16]='M'
     #f_name="".join(chArr)    
@@ -71,20 +72,30 @@ for i in range(LL):
     #chArr[16]='B'
     #f_name="".join(chArr)    
     #l_Bar.append(genfromtxt(f_name, delimiter=','))
+    res = res.append(df, ignore_index=True)
+    print(f'File  {i+1}  out of {len(FileNames)} shape {res.shape}')
 
-    print(f'File  {i+1}  out of {len(FileNames)}')
 os.chdir(oldDir)
 
-#This create the panda dataframe
-FallAllD = df
+#Debugging go through all the list and check the data type of the elements
+#print(type(l_Mag[0]))
+#print(type(l_Bar[0]))
 
-# Debuggin Tool: extract the data from the dataframe and use columns 'SubjectID', 'Device','ActivityID','TrialNo','Acc'(3),'Gyr'(3)
-def extract_data(df, col_name):
-    print(df[col_name].values.tolist())
-    return df[col_name].values.tolist()
+
+#This create the panda dataframe
+FallAllD = res
+#  extract the data from the dataframe and use columns 'SubjectID', 'Device','ActivityID','TrialNo','Acc','Gyr'
+# subjectID = FallAllD['SubjectID']
+# device = FallAllD['Device']
+# activityID = FallAllD['ActivityID']
+# trialNo = FallAllD['TrialNo']
+# acc = FallAllD['Acc']
+# gyr = FallAllD['Gyr']
+
 
 #Pickle: Serializing and deserializing a Python object structure
-FallAllD.to_pickle('FallAllD.pkl')
+#Format: {SubjectID, ActivityID, TrialNo, Device, Acc, Gyr, Mag, Bar}
+FallAllD.to_pickle('FallAllD2.pkl')
 
 #NOT USING. Write the contained data to an HDF5 file using HDFStore.
 #FallAllD.to_hdf('FallAllD.h5', key='df', mode='w')
